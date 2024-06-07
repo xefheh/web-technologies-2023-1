02f81c09bd9751c556487eb3a1990ee550e65cb8
xefheh <09092002Xefr@>
2024-06-07 19:56:13 +0500

    готово

--- index.php
+++ index.php
@@ -1,39 +1,84 @@
 <?php
-    $title = 'Крутой Title';
-    $h1 = 'Крутой h1';
-    $year = date('Y');
-    $time = time();
-    function getTimeString($time) {
-        $hourForms = ['час', 'часа', 'часов'];
-        $minutesForms = ['минута', 'минуты', 'минут'];
-        $hours = intval(date('H', $time));
-        $minutes = intval(date('i', $time));
-        return $hours . ' ' . getStringForm($hours, $hourForms) . 
-            ' ' . $minutes . ' ' . getStringForm($minutes, $minutesForms);
+echo ' 1 квест. <br>';
+$a = 3;
+$b = 5;
+
+if($a < 0 && $b < 0) {
+    echo $a * $b;  
+} else if ($a >= 0 && $b >= 0) {
+    echo $a * $b;
+} else {
+    echo $a + $b;
+}
+
+echo '<br>';
+echo ' 2 квест. <br>';
+
+
+$a = 13;
+switch ($a) {
+    case 0: echo 0 . '<br>';
+    case 1: echo 1 . '<br>';
+    case 2: echo 2 . '<br>';
+    case 3: echo 3 . '<br>';
+    case 4: echo 4 . '<br>';
+    case 5: echo 5 . '<br>';
+    case 6: echo 6 . '<br>';
+    case 7: echo 7 . '<br>';
+    case 8: echo 8 . '<br>';
+    case 9: echo 9 . '<br>';
+    case 10: echo 10 . '<br>';
+    case 11: echo 11 . '<br>';
+    case 12: echo 12 . '<br>';
+    case 13: echo 13 . '<br>';
+    case 14: echo 14 . '<br>';
+    case 15:
+        echo 15 . '<br>';
+        break;
+}
+
+echo '<br>';
+
+echo ' 3 квест. <br>';
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    return $a / $b;
+}
+
+function substract($a, $b) {
+    return $a - $b;
+}
+
+echo ' 4 квест. <br>';
+
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case 'сложение':
+            return $arg1 + $arg2;
+        case 'вычитание':
+            return $arg1 - $arg2;
+        case 'умножение':
+            return $arg1 * $arg2;
+        case 'деление':
+            return $arg1 / $arg2;
+        default:
+            return 'капец';
     }
-    
-    function getStringForm($number, $forms) {
-        if($number % 10 == 1 && $number % 100 != 11) {
-            return $forms[0];
-        } else if(($number % 10 > 2 && $number % 10 < 4) &&
-            ($number % 100 < 10 || $number % 100 >= 20)) {
-            return $forms[1];
-        }
-        return $forms[2];
-    }
+}
+
+echo ' 6 квест. <br>';
+function power($a, $b) {
+    if($b <= 0) return 1;
+    return $a * power($a, --$b);
+}
+
+echo power(5, 3);
 ?>
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $title ?></title>
-</head>
-<body>
-    <h1><?php echo $h1 ?></h1>
-    <p><?php echo $year ?></p>
-    <p><?php echo getTimeString($time) ?></p>
-</body>
-</html>
